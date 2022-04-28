a718ca34e0d8c77577464be8cd4515040dff42a4
Vicente Alvaro <vicenalvaro@gmail.com>
2022-04-29 00:16:37 +0200

    Loader bar

---
+++ src/base/b-loaderBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="loaderBar"></div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.loaderBar {
+  width: 100%;
+  background-color: red;
+  height: 2px;
+}
+</style>
