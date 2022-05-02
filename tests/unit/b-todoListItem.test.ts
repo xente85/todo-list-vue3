91f5bf2a92577756bc9da23eaa941d62a8dd8696
Vicente Alvaro <vicenalvaro@gmail.com>
2022-05-02 19:06:02 +0200

    Test todo item complete

--- tests/unit/b-todoListItem.test.ts
+++ tests/unit/b-todoListItem.test.ts
@@ -54,4 +54,41 @@ describe('bTodoListItem', () =>
     expect(emitStatus.changeItemStatus[0][0]).toHaveProperty('status')
     expect(emitStatus.changeItemStatus[0][0].status).equals(!mockItem.completed)
   })
+
+  it('Item change text', async () =>
+  {
+    const { container, emitted } = await render(bTodoListItem, { props: { item: mockItem } })
+
+    const labelText = container.querySelector('label')
+    await fireEvent.dblClick(labelText)
+    const inputEdit = container.querySelector('.edit')
+    expect(inputEdit).toBeTruthy()
+
+    const newText = 'new text'
+    await fireEvent.update(inputEdit, newText)
+
+    // Key press
+    await fireEvent.keyDown(inputEdit, { key: 'Enter', keyCode: 13 })
+    const emitTextEnter = emitted()
+    expect(emitTextEnter).toHaveProperty('changeItemText')
+    expect(emitTextEnter.changeItemText).toHaveLength(1)
+    expect(emitTextEnter.changeItemText[0][0]).toHaveProperty('id')
+    expect(emitTextEnter.changeItemText[0][0].id).equals(mockItem.id)
+    expect(emitTextEnter.changeItemText[0][0]).toHaveProperty('text')
+    expect(emitTextEnter.changeItemText[0][0].text).equals(newText)
+
+    // On blur
+    await fireEvent.blur(inputEdit)
+    const emitTextBlur = emitted()
+    expect(emitTextBlur).toHaveProperty('changeItemText')
+    expect(emitTextBlur.changeItemText).toHaveLength(1)
+    expect(emitTextBlur.changeItemText[0][0]).toHaveProperty('id')
+    expect(emitTextBlur.changeItemText[0][0].id).equals(mockItem.id)
+    expect(emitTextBlur.changeItemText[0][0]).toHaveProperty('text')
+    expect(emitTextBlur.changeItemText[0][0].text).equals(newText)
+
+    // Escape
+    await fireEvent.keyDown(inputEdit, { key: 'Esc', keyCode: 27 })
+    expect(inputEdit.value).equals(mockItem.text)
+  })
 })
